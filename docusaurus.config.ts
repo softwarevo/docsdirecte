c440063972fc0c62cef2b73adf2165a70f912c69
Adouche <mr.froid.84@gmail.com>
2026-04-10 18:54:18 +0200

    chore: Update sitemap settings

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -39,6 +39,12 @@ const config: Config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        sitemap: {
+          lastmod: "date",
+          changefreq: "weekly",
+          priority: 0.5,
+          filename: "sitemap.xml",
+        },
       } satisfies Preset.Options,
     ],
   ],
